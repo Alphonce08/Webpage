9b118f471b44216c1b14c636d14522790c224049
Mike Cheruiyot <65896352+kimutai-debug@users.noreply.github.com>
2024-05-24 02:04:48 +0300

    new page

--- myapp/urls.py
+++ myapp/urls.py
@@ -3,20 +3,12 @@ from . import views
 
 
 app_name = "myapp"
-
 urlpatterns = [
-
     path('', views.index, name='index'),
+    path('soft', views.soft, name='soft'),
+    path('team', views.team, name='team'),
     path('fash', views.fash, name='fash'),
-    path('soft', views.soft,  name='soft'),
-<<<<<<< HEAD
-    path('about', views.about,  name='about'),
-    path('blog', views.blog,  name='blog'),
-    path('classes', views.classes,  name='classes'),
-    path('sale', views.sale,  name='sale')
-=======
-    path('team', views.team,  name='team'),
-    path('about', views.about,  name='about'),
-    path('blog', views.blog,  name='blog')
->>>>>>> af6514201eb3e25aeadbdd1ca280b23217cb423a
+    path('about', views.about, name='about'),
+    path('blog', views.blog, name='blog'),
+    path('classes', views.classes, name='classes')
     ]
--- myapp/views.py
+++ myapp/views.py
@@ -6,34 +6,15 @@ from django.http import HttpResponse
 
 def index(request):
     return render(request, 'index.html')
-
 def fash(request):
     return render(request, 'fash.html')
-
 def soft(request):
     return render(request, 'soft.html')
-
-
 def about(request):
     return render(request, 'about.html')
-
-
-<<<<<<< HEAD
-def classes(request):
-    return render(request, 'classes.html')
-
-
-
-def blog(request):
-    return render(request, 'blog.html')
-
-
-def sale(request):
-    return render(request, 'sale.html')
-=======
 def team(request):
     return render(request, 'team.html')
-
 def blog(request):
     return render(request, 'blog.html')
->>>>>>> af6514201eb3e25aeadbdd1ca280b23217cb423a
+def classes(request):
+    return render(request, 'classes.html')
